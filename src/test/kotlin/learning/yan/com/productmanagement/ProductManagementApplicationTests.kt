19ff7c7c654f4f52997d74bc9854c424425f0769
Yan Zingra Pereira <yan.zingra12@gmail.com>
2023-12-05 21:57:17 -0800

    Test first pipeline

--- src/test/kotlin/learning/yan/com/productmanagement/ProductManagementApplicationTests.kt
+++ src/test/kotlin/learning/yan/com/productmanagement/ProductManagementApplicationTests.kt
@@ -1,13 +1,14 @@
 package learning.yan.com.productmanagement
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class ProductManagementApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		Assertions.assertEquals(1,1)
 	}
 
 }

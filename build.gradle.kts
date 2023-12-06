c31ab5335adc09836b26b0ebf3af6d0e8a923ed2
Yan Zingra Pereira <yan.zingra12@gmail.com>
2023-12-05 22:28:08 -0800

    Test first pipeline

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import com.adarshr.gradle.testlogger.TestLoggerExtension
+import com.adarshr.gradle.testlogger.theme.ThemeType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.springframework.boot") version "3.2.0"
     id("io.spring.dependency-management") version "1.1.4"
+    id("com.adarshr.test-logger") version "4.0.0" apply true
     kotlin("jvm") version "1.9.20"
     kotlin("plugin.spring") version "1.9.20"
     kotlin("plugin.jpa") version "1.9.20"
@@ -45,3 +48,9 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
+configure<TestLoggerExtension> {
+    theme = ThemeType.MOCHA
+    showExceptions = true
+}
+
--- src/test/kotlin/learning/yan/com/productmanagement/ProductManagementApplicationTests.kt
+++ src/test/kotlin/learning/yan/com/productmanagement/ProductManagementApplicationTests.kt
@@ -2,13 +2,12 @@ package learning.yan.com.productmanagement
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
 class ProductManagementApplicationTests {
 
 	@Test
 	fun contextLoads() {
-		Assertions.assertEquals(1,1)
+		Assertions.assertEquals(12,12)
 	}
 
 }
